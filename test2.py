080af64a707dff1796ae6994380b0f4e16f8f97a
zmscgsf <2010909388@qq.com>
2019-10-24 13:52:26 +0800

    second commit_ssh

---
+++ test2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+print('hello world')
+for i in range(1,9,2):
+    print(i)
+
+#%%
+
+import math
+def    Area (r):
+       s=math.pi*r**2
+       return s
+
+r = 25
+print(Area(r))
+print(Area(10))
+
+
+#%%
