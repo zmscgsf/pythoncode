106285d9a1049b8c82112322a2a88054c896adba
zmscgsf <2010909388@qq.com>
2019-10-24 22:26:44 +0800

    ssh git  del jupyter preview

--- test2.py
+++ test2.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
+# -*- coding: UTF-8 ;-*-
 print('hello world')
 for i in range(1,9,2):
     print(i)
 
-#%%
-
 import math
-def    Area (r):
-       s=math.pi*r**2
-       return s
+
+def Area (r):
+    s=math.pi*r**2
+    return s
 
 r = 25
 print(Area(r))
 print(Area(10))
-
-
-#%%
